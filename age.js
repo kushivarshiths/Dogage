7ace34c81ced670493be19faf87cc38fba647fcc
dztrctstar <81169621+kushivarshiths@users.noreply.github.com>
2021-09-06 20:54:52 +0530

    Create age.js

---
+++ age.js
@@ -0,0 +1,12 @@
+let myAge = 19; //present age
+let earlyYears = 2;//early years
+earlyYears *= 10.5;
+
+let laterYears = myAge;//lateryears is lateryears of the dog
+laterYears -= 2;
+laterYears *= 4;
+console.log(`Early years: ${earlyYears}`);
+console.log(`Later years: ${laterYears}`);
+let myAgeInDogYears = earlyYears + laterYears;
+let myName = 'Kushi Varshith'.toLowerCase();
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
